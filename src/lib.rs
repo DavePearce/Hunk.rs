e18f2a7cf230de2b477d87c0efb7bc1df253c2c6
DavePearce <david.pearce@ecs.vuw.ac.nz>
2021-09-20 18:13:31 +1200

    Very initial draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,15 @@
 // Stuf goes here
+pub const EMPTY : ByteHunk<'static> = ByteHunk{ bytes: &[] };
+
+/// Represents a hunk of raw data constructed from an in-memory byte
+/// array.
+pub struct ByteHunk<'a> {
+    pub bytes: &'a [u8]
+}
+
+impl<'a> ByteHunk<'a> {
+    pub fn new(bytes: &'a [u8]) -> ByteHunk<'a> {
+	ByteHunk{bytes}
+    }
+}
+
